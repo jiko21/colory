05dfc2a3087ad153c84d32f401f368e4822b5d32
jiko21 <daikikojima0531@icloud.com>
2021-07-14 00:02:19 +0900

    add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  extends: ['plugin:react/recommended', 'google', 'prettier', 'plugin:jest/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -12,9 +12,10 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'jest'],
   rules: {
-    "require-jsdoc": "off"
+    "require-jsdoc": "off",
+    "react/react-in-jsx-scope": "off"
   },
   settings: {
     react: {
