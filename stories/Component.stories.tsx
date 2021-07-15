d046aef5c9359d270d6e23407b5e17fcd014f590
jiko21 <daikikojima0531@icloud.com>
2021-07-15 23:09:01 +0900

    use @ path alias

--- stories/Component.stories.tsx
+++ stories/Component.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import Component from '../src/Component';
+import Component from '@/Component';
 
 export default {
   title: 'Component',
