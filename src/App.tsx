064f52d4adc38e20bd739793d8210dccdac3b2f7
jiko21 <daikikojima0531@icloud.com>
2021-07-20 21:27:19 +0900

    apply header

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
-import React, { useState } from 'react';
-import { Provider, defaultTheme } from '@adobe/react-spectrum';
+import React from 'react';
+import { Provider, defaultTheme, Text } from '@adobe/react-spectrum';
+import DefaultTemplate from '@/components/Templates/DefaultTemplate';
+import Header from '@/components/Organisms/Header';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
     <Provider theme={defaultTheme}>
-      <div className="App">
-        <header className="App-header">
-          <p>Hello Vite + React!</p>
-          <p>
-            <button type="button" onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </header>
-      </div>
+      <DefaultTemplate>
+        <Header>
+          <Text height="size-1000" margin="size-1000">
+            Colory
+          </Text>
+        </Header>
+      </DefaultTemplate>
     </Provider>
   );
-}
+};
 
 export default App;
