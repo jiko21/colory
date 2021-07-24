6f206c1987c62451cefbd5b6265f13c047386db8
jiko21 <daikikojima0531@icloud.com>
2021-07-24 16:46:25 +0900

    fix eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,16 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    browser: true
+    browser: true,
   },
-  extends: ['plugin:react/recommended', 'google', 'prettier', 'plugin:jest/recommended', 'eslint:recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'google',
+    'prettier',
+    'plugin:jest/recommended',
+    'eslint:recommended',
+    'react-hooks',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -15,14 +22,14 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'jest'],
   rules: {
-    "no-console": "error",
-    "require-jsdoc": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": [2, { ignore: ['children'] }]
+    'no-console': 'error',
+    'require-jsdoc': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
   settings: {
     react: {
       version: 'detect',
-    }
-  }
+    },
+  },
 };
